fa97b4eb69d3f3353f0773b5c4b3d58e23864cbf
Rae <raemmacias@gmail.com>
2018-10-26 07:44:26 -0600

    Attempting saveInstanceState for ExoPlayer, not working

--- app/src/main/java/com/raemacias/thebakingapp/fragments/RecipeStepFragment.java
+++ app/src/main/java/com/raemacias/thebakingapp/fragments/RecipeStepFragment.java
@@ -48,9 +48,14 @@ public class RecipeStepFragment extends Fragment {
    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
    private static final String TAG = "PlayerActivity";
 
+    public static final String AUTOPLAY = "autoplay";
+    public static final String CURRENT_WINDOW_INDEX = "current_window_index";
+    public static final String PLAYBACK_POSITION = "playback_position";
+
    private SimpleExoPlayer player;
    private SimpleExoPlayerView playerView;
-   private ComponentListener componentListener;
+//   private ComponentListener componentListener;
+   private boolean autoPlay = false;
 
    private long playbackPosition;
    private int currentWindow;
@@ -81,10 +86,18 @@ public class RecipeStepFragment extends Fragment {
         recipeInstruction = rootView.findViewById(R.id.tv_step_description);
         recipeInstruction.setText(stepDescription);
 
-        componentListener = new ComponentListener();
+//        componentListener = new ComponentListener();
         playerView = rootView.findViewById(R.id.video_view);
 
-        initializePlayer();
+        if (savedInstanceState != null) {
+            playbackPosition = savedInstanceState.getLong(PLAYBACK_POSITION, 0);
+            currentWindow = savedInstanceState.getInt(CURRENT_WINDOW_INDEX, 0);
+            autoPlay = savedInstanceState.getBoolean(AUTOPLAY, false);
+        }
+
+
+            initializePlayer();
+
 
         return rootView;
 
@@ -132,9 +145,9 @@ public class RecipeStepFragment extends Fragment {
             player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(getContext()),
             new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
 
-            player.addListener(componentListener);
-            player.setVideoDebugListener(componentListener);
-            player.setAudioDebugListener(componentListener);
+//            player.addListener(componentListener);
+//            player.setVideoDebugListener(componentListener);
+//            player.setAudioDebugListener(componentListener);
             playerView.setPlayer(player);
             player.setPlayWhenReady(playWhenReady);
             player.seekTo(currentWindow, playbackPosition);
@@ -157,16 +170,34 @@ public class RecipeStepFragment extends Fragment {
             }
         }
 
+
     private void releasePlayer() {
         if (player != null) {
             playbackPosition = player.getCurrentPosition();
             currentWindow = player.getCurrentWindowIndex();
             playWhenReady = player.getPlayWhenReady();
-            player.removeListener(componentListener);
+//            player.removeListener(componentListener);
             player.setVideoListener(null);
             player.setVideoDebugListener(null);
             player.release();
             player = null;
+        }
+    }
+
+    //This came from this gist: https://gist.github.com/codeshifu/c26bb8a5f27f94d73b3a4888a509927c
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        /*
+         * A simple configuration change such as screen rotation will destroy this activity
+         * so we'll save the player state here in a bundle (that we can later access in onCreate) before everything is lost
+         * NOTE: we cannot save player state in onDestroy like we did in onPause and onStop
+         * the reason being our activity will be recreated from scratch and we would have lost all members (e.g. variables, objects) of this activity
+         */
+        if (player == null) {
+            outState.putLong(PLAYBACK_POSITION, playbackPosition);
+            outState.putInt(CURRENT_WINDOW_INDEX, currentWindow);
+            outState.putBoolean(AUTOPLAY, autoPlay);
         }
     }
 
@@ -187,134 +218,119 @@ public class RecipeStepFragment extends Fragment {
     }
 
 
+//    public class ComponentListener implements ExoPlayer.EventListener, VideoRendererEventListener,
+//            AudioRendererEventListener{
+//
+//        @Override
+//        public void onTimelineChanged(Timeline timeline, Object manifest) {
+//            //do nothing
+//        }
+//
+//        @Override
+//        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+//            //do nothing
+//        }
+//
+//        @Override
+//        public void onLoadingChanged(boolean isLoading) {
+//            //do nothing
+//        }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
+//        @Override
+//        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+//            String stateString;
+//            switch (playbackState) {
+//                case ExoPlayer.STATE_IDLE:
+//                    stateString = "ExoPlayer.STATE_IDLE -";
+//                    break;
+//                    case ExoPlayer.STATE_BUFFERING:
+//                        stateString = "ExoPlayer.STATE_BUFFERING -";
+//                        break;
+//                        case ExoPlayer.STATE_READY:
+//                            stateString = "ExoPlayer.STATE_READY -";
+//                            break;
+//                            case ExoPlayer.STATE_ENDED:
+//                                stateString = "ExoPlayer.STATE_ENDED -";
+//                                break;
+//                                default:
+//                                    stateString = "UNKNOWN_STATE    -";
+//                                    break;
+//
+//            }
+//            Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
+//
+//        }
+
+
+//        @Override
+//        public void onPlayerError(ExoPlaybackException error) {
+//            //do nothing
+//        }
+//
+//        @Override
+//        public void onPositionDiscontinuity() {
+//            //do nothing
+//        }
+//
+//        @Override
+//        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+//            //do nothing
+//        }
+//
+//        @Override
+//        public void onAudioEnabled(DecoderCounters counters) {
+//        }
+//
+//        @Override
+//        public void onAudioSessionId(int audioSessionId) {
+//
+//        }
+//
+//        @Override
+//        public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+//
+//        }
+//
+//        @Override
+//        public void onAudioInputFormatChanged(Format format) {
+//
+//        }
+//
+//        @Override
+//        public void onAudioTrackUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
+//        }
+//
+//        @Override
+//        public void onAudioDisabled(DecoderCounters counters) {
+//
+//        }
+//
+//        @Override
+//        public void onVideoEnabled(DecoderCounters counters) {
+//        }
+//
+//        @Override
+//        public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
+//        }
+//
+//        @Override
+//        public void onVideoInputFormatChanged(Format format) {
+//        }
+//
+//        @Override
+//        public void onDroppedFrames(int count, long elapsedMs) {
+//        }
+//
+//        @Override
+//        public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+//        }
+//
+//        @Override
+//        public void onRenderedFirstFrame(Surface surface) {
+//        }
+//
+//        @Override
+//        public void onVideoDisabled(DecoderCounters counters) {
+//        }
 //    }
-
-    public class ComponentListener implements ExoPlayer.EventListener, VideoRendererEventListener,
-            AudioRendererEventListener{
-
-        @Override
-        public void onTimelineChanged(Timeline timeline, Object manifest) {
-            //do nothing
-        }
-
-        @Override
-        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-            //do nothing
-        }
-
-        @Override
-        public void onLoadingChanged(boolean isLoading) {
-            //do nothing
-        }
-
-        @Override
-        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-            String stateString;
-            switch (playbackState) {
-                case ExoPlayer.STATE_IDLE:
-                    stateString = "ExoPlayer.STATE_IDLE -";
-                    break;
-                    case ExoPlayer.STATE_BUFFERING:
-                        stateString = "ExoPlayer.STATE_BUFFERING -";
-                        break;
-                        case ExoPlayer.STATE_READY:
-                            stateString = "ExoPlayer.STATE_READY -";
-                            break;
-                            case ExoPlayer.STATE_ENDED:
-                                stateString = "ExoPlayer.STATE_ENDED -";
-                                break;
-                                default:
-                                    stateString = "UNKNOWN_STATE    -";
-                                    break;
-
-            }
-            Log.d(TAG, "changed state to " + stateString + " playWhenReady: " + playWhenReady);
-
-        }
-
-        @Override
-        public void onPlayerError(ExoPlaybackException error) {
-            //do nothing
-        }
-
-        @Override
-        public void onPositionDiscontinuity() {
-            //do nothing
-        }
-
-        @Override
-        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-            //do nothing
-        }
-
-        @Override
-        public void onAudioEnabled(DecoderCounters counters) {
-        }
-
-        @Override
-        public void onAudioSessionId(int audioSessionId) {
-
-        }
-
-        @Override
-        public void onAudioDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-
-        }
-
-        @Override
-        public void onAudioInputFormatChanged(Format format) {
-
-        }
-
-        @Override
-        public void onAudioTrackUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
-        }
-
-        @Override
-        public void onAudioDisabled(DecoderCounters counters) {
-
-        }
-
-        @Override
-        public void onVideoEnabled(DecoderCounters counters) {
-        }
-
-        @Override
-        public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-        }
-
-        @Override
-        public void onVideoInputFormatChanged(Format format) {
-        }
-
-        @Override
-        public void onDroppedFrames(int count, long elapsedMs) {
-        }
-
-        @Override
-        public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
-        }
-
-        @Override
-        public void onRenderedFirstFrame(Surface surface) {
-        }
-
-        @Override
-        public void onVideoDisabled(DecoderCounters counters) {
-        }
-    }
 }
